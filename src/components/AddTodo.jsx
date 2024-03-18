06c415c9beddf8b4d03b343d6f2c6205cd5a88e7
Ankit <ankitmittal0814@gmail.com>
2024-03-19 00:17:05 +0530

    Update AddTodo.jsx

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -38,7 +38,10 @@ function AddTodo() {
   };
 
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12 mb-8">
+    <form
+      onSubmit={addTodoHandler}
+      className="space-x-3 mt-12 mb-8  sm:mt-12  sm:flex-row items-center space-y-4 sm:space-y-0"
+    >
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
